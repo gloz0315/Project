7c6f9d4d66bffce19e385d9da729c75741519a2a
gloz0315 <qudtn0315@naver.com>
2024-01-02 18:53:56 +0900

    리팩토링 및 주문 내역 사는 로직 추가

--- src/project1/controller/MainController.java
+++ src/project1/controller/MainController.java
@@ -18,12 +18,14 @@ public class MainController {
     private MenuListInit menuListInit;      // 종류별 음식들의 정보들을 초기화 해주는 클래스
     private ProductList productList;        // 제품별 정보를 저장하는 인터페이스인 ProductList를 이용할 것임
     private MenuController menuController;  // 특정 종류의 음식을 선택할 때 사용되는 로직을 담당하는 클래스
+    private BuyController buyController;    // 총 주문 정보들을 사려고 할때 처리하는 로직을 담당하는 클래스
 
     public MainController() {
         orderMap = new LinkedHashMap<>();
         mainMenuOutput = new MainMenuOutput();
         menuListInit = new MenuListInit();
         menuController = new MenuController();
+        buyController = new BuyController(orderMap);
     }
 
     public void start() {
@@ -39,15 +41,15 @@ public class MainController {
 
     private void menuCase(int number) {
         switch (number) {
-            case 1 ->         // 버거
+            case 1 ->
                     productList = menuListInit.burgerMenu();
-            case 2 ->         // 감자튀김
+            case 2 ->
                     productList = menuListInit.friedMenu();
-            case 3 ->         // 음료
+            case 3 ->
                     productList = menuListInit.iceCreamMenu();
-            case 4 ->         // 맥주
+            case 4 ->
                     productList = menuListInit.beerMenu();
-            case 5 -> {         // 장바구니
+            case 5 -> {         // 주문 버튼 (이거는 하나의 컨트롤러 클래스를 따로 정리해서 만들어야겠다.)
                 printBuyList();
                 return;
             }
@@ -72,14 +74,42 @@ public class MainController {
 
     // 장바구니에 대한 정보 출력
     private void printBuyList() {
+        float total = 0.0f;
+        System.out.println("다음과 같이 주문하시겠습니까?\n\n");
+
+        System.out.println("[ Orders ]");
         for(Entry<Item,Integer> items : orderMap.entrySet()) {
             System.out.println("음식 이름:" + items.getKey().getName() + " 음식 가격:" + items.getKey().itemPrice()
              + " 음식 설명:" + items.getKey().getDescription());
+            total += items.getKey().itemPrice();
         }
+
+        System.out.println();
+        System.out.println("[ Total ] ");
+        System.out.println("W " + total + "\n");
+        System.out.println("1. 주문     2. 메뉴판");
     }
     
+    // 장바구니에 대한 정보들을 다 삭제할 것인지에 대한 메서드
     private void clearOrderMenu() {
-        orderMap.clear();           // orderMap에 대한 정보들을 다 제거
-        System.out.println("모든 주문이 취소되었습니다.\n");
+        if(isClearOrder()) {
+            orderMap.clear();           // orderMap에 대한 정보들을 다 제거
+            System.out.println("모든 주문이 취소되었습니다.\n");
+        }
+
+    }
+
+    private boolean isClearOrder() {
+        while(true) {
+            System.out.println("주문을 취소 하시겠습니까?");
+            System.out.println("1. 확인       2. 취소");
+            String text = InputView.input();
+            System.out.println();
+
+            if(text.equals("1"))
+                return true;
+            else if(text.equals("2"))
+                return false;
+        }
     }
 }
