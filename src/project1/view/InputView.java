b17a37ace8c96e9c3742e6922b75fc38d39eafa2
gloz0315 <qudtn0315@naver.com>
2024-01-03 09:50:28 +0900

    InputView에 대한 생성자 제한 설정 (static 메서드만 있으므로)

--- src/project1/view/InputView.java
+++ src/project1/view/InputView.java
@@ -4,6 +4,10 @@ import project1.console.Console;
 
 public class InputView {
 
+    private InputView() {
+        // 생성자를 막음으로서 인스턴스 생성을 막음 (결국에 static 메서드만 잇으니까)
+    }
+
     public static String input() {
         System.out.print("번호를 입력하세요 > ");
         return Console.readLine();
